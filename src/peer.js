b0163fd72cd4693aac1c9fd43b64bd43b942635b
ankit4442 <142345891+ankit4442@users.noreply.github.com>
2026-07-18 21:32:22 +0530

    Refactor createPeerConnection for better clarity

--- src/peer.js
+++ src/peer.js
@@ -1,47 +1,3 @@
-// export const createPeerConnection = (
-//   localStream,
-//   remoteVideoRef,
-//   socket,
-//   roomId
-// ) => {
-//   const peer = new RTCPeerConnection({
-//     iceServers: [
-//       {
-//         urls: "stun:stun.l.google.com:19302",
-//       },
-//     ],
-//   });
-
-//   const remoteStream = new MediaStream();
-
-//   if (remoteVideoRef.current) {
-//     remoteVideoRef.current.srcObject = remoteStream;
-//   }
-
-//   peer.ontrack = (event) => {
-//     event.streams[0].getTracks().forEach((track) => {
-//       remoteStream.addTrack(track);
-//     });
-//   };
-
-//   peer.onicecandidate = (event) => {
-//     if (event.candidate) {
-//       socket.emit("ice-candidate", {
-//         roomId,
-//         candidate: event.candidate,
-//       });
-//     }
-//   };
-
-//   localStream.getTracks().forEach((track) => {
-//     peer.addTrack(track, localStream);
-//   });
-
-//   return peer;
-// };
-
-
-
 export const createPeerConnection = ({
   localStream,
   remoteVideoRef,
@@ -198,9 +154,5 @@ export const createPeerConnection = ({
   return peer;
 };
  
-
-
-
-
 
 
